bf4b8bce3365776eb0579ae1c642219930d4ad2f
Patrick Lee Scott <pat@patscott.io>
2022-10-24 21:44:55 -0500

    feat: updates

--- src/handlers/todo.removed.ts
+++ src/handlers/todo.removed.ts
@@ -1,5 +1,9 @@
 import axios from "axios";
 import { config } from "../config.js";
+import { hasuraAndKnative } from "../lib/handlers.js";
+import axiosRetry from "axios-retry";
+
+axiosRetry(axios, { retries: 3 });
 
 const HASURA_OPERATION_COMPLETE_TODO = `
 mutation RemoveTodo(
@@ -39,46 +43,4 @@ const removeTodo = async (removedTodo) => {
   };
 };
 
-export const handle = async (
-  request,
-  response,
-  event,
-  handlerOptions = { sync: false }
-) => {
-  const { sync } = handlerOptions;
-  const { data, type } = event;
-  const { completedDenormalizers, id } = data;
-  const hasBeenProcessed =
-    completedDenormalizers &&
-    completedDenormalizers.includes(config.denormalizer);
-
-  request.log.info({ msg: `⏳ handling ${type}`, data, hasBeenProcessed });
-
-  if (hasBeenProcessed) {
-    request.log.info({
-      msg: "🛑 this message has already been processed via the sync handler",
-      id,
-    });
-
-    if (sync) {
-      return response.status(202).json({ id });
-    } else {
-      return response.status(202).send();
-    }
-  } else {
-    request.log.info({ msg: `⏳ denormalizing ${type}`, data, event });
-    const hasuraResult = await removeTodo(data);
-
-    if (hasuraResult.errors) {
-      request.log.info({ msg: "🚨 hasura error", hasuraResult });
-      response.status(400).json(hasuraResult);
-    } else {
-      request.log.info({ msg: "✅ hasura result", hasuraResult });
-      if (sync) {
-        return response.status(hasuraResult.status).json(hasuraResult);
-      } else {
-        return response.status(202).send();
-      }
-    }
-  }
-};
+export const handle = hasuraAndKnative(removeTodo);
